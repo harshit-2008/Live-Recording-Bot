8fa8a3525507b4d2e536a186579a48b46cc09496
Harshit-2005 <168950439+harshit-2008@users.noreply.github.com>
2024-07-15 14:55:29 +0530

    Update Config.py

--- Config.py
+++ Config.py
@@ -1,15 +1,8 @@
 import os
 
-# Bot token from @botfather
-BOT_TOKEN = os.environ.get("BOT_TOKEN")
-# From my.telegram.org/
-API_ID = int(os.environ.get("API_ID", 0))
-API_HASH = os.environ.get("API_HASH")
-# For /log cmd
-OWNER_ID = [int(i) for i in os.environ.get("OWNER_ID", "1204927413").split(" ")]
-# No time limit for this users
-AUTH_USERS = [int(i) for i in os.environ.get("AUTH_USERS", "1204927413").split(" ")]
-# Time gap after each request (in seconds)
-TIME_GAP = int(os.environ.get("TIME_GAP", "360"))
-# Bot channel ID for ForceSub, don't forget to add bot in Bot Channel
-UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", False)
+os.environ['API_ID'] = '27190467'
+os.environ['API_HASH'] = 'ff6bc6ad2faba520f426cf04ca7f5773'
+os.environ['BOT_TOKEN'] = '7439562089:AAERgxvEYiLJF_juL68k1nn78negwJ3mNiM'
+os.environ['DATABASE_DUMP_CHANNEL'] = '-1002160780409'
+os.environ['SUDO_USERS'] = '6066102279'
+os.environ['FFMPEG_BIN'] = '/usr/bin/ffmpeg'  # Path to ffmpeg executable
